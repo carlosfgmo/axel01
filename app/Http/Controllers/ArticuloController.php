ccfdb551964958aeb8596655e5423b168940695c
unknown <carlosfgmo@gmail.com>
2022-06-25 18:01:04 -0500

    vue-ok

---
+++ app/Http/Controllers/ArticuloController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Articulo;
+use Illuminate\Http\Request;
+
+class ArticuloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articulos = Articulo::get();
+        return response()->json([
+            'articulos' => $articulos
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $articulo->Articulo::create($request->post());
+        return response()->json([
+            'articulo' => $articulo,
+            'success' => true
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Articulo  $articulo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Articulo $articulo)
+    {
+        return response()->json($articulo);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Articulo  $articulo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Articulo $articulo)
+    {
+        $articulo->update($request->post());
+        return response()->json([
+            'articulo' => $articulo,
+            'success' => true
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Articulo  $articulo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Articulo $articulo)
+    {
+        $articulo->delete();
+        return response()->json([
+            'success' => true
+        ]);
+    }
+}
